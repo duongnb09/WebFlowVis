68792a168552ed16b1c2f7bfd9786c8172f7bde3
davim <davim@davim.cs.uh.edu>
2019-01-21 00:04:11 -0600

    Add the update.php file to automatically pull the latest update from github to local repository

---
+++ update.php
@@ -0,0 +1,31 @@
+<?php
+$output = shell_exec('git pull');
+//echo "<pre>$output</pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>WebFlowVis Update</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="page-header">
+    <h1>Update WebFlowVis project from Github</h1>      
+  </div>
+
+  <div class="alert alert-success">
+  <strong>Success!</strong> Your WebFlowVis folder has been updated successfully.
+</div>
+
+  <button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='../WebFlowVis/'">Click here to see results</button>
+</div>
+
+</body>
+</html>
