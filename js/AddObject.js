41932c43dd6e60ae62b20bcf110887836074fc6e
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-05 14:03:26 -0600

    Added Transparency and Color

--- js/AddObject.js
+++ js/AddObject.js
@@ -1,12 +1,19 @@
 function AddObject(name2){
-	console.log("run");
-	var loader = new THREE.ObjectLoader();
+	var loader = new THREE.OBJLoader();
 	loader.load(name2, function ( object ) {
-		object.castShadow() = true;
-		//console.log(object.children.size);
-		//scene.add( object );},
+		console.log(name2);
+    	object.traverse( function ( child ) {
+        	if ( child instanceof THREE.Mesh ) {
+				console.log("found mesh");
+            	child.material.side = THREE.BackSide;
+            	child.material.color.setHex(0x808080);
+				child.material.transparent = true;
+				child.material.opacity = .5;
+        	}
+    	} );
+		scene.add( object );},
 	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		
 	},
 	function ( error ) {
 		alert( 'An error happened' );
